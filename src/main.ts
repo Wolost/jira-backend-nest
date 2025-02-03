979d1ea2a9ebb783ccd64f309d434668c51225ad
Alejandro Muñoz <86081720+Wolost@users.noreply.github.com>
2025-02-03 10:18:22 -0500

    Test railway config.

--- src/main.ts
+++ src/main.ts
@@ -30,8 +30,7 @@ async function bootstrap() {
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('api/docs', app, document);
 
-    //const port = process.env.PORT || 3000;
-    const port = process.env.PORT;
+    const port = process.env.PORT || 3000;
     await app.listen(port);
 }
 
