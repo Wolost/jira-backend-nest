c89b7d41b24b4cc7126798fe7ab68b9248e7510e
Alejandro Muñoz <86081720+Wolost@users.noreply.github.com>
2025-02-04 20:07:18 -0500

    Added seeds to populate all the database.

--- src/database/seeds/lov.seed.ts
+++ src/database/seeds/lov.seed.ts
@@ -22,9 +22,37 @@ async function main() {
       firstName: 'Admin',
       lastName: 'User',
       email: 'admin@example.com',
-      password: 'securepassword',
+      password: '$2b$10$IePM/c3xQSsFosDXTt8HHOpJbc0aRaSmKCbu88JLoKGc5m9TDASA2',
       role: 'admin',
       language: 'en-US',
+    },
+  });
+
+  await prisma.projects.create({
+    data: {
+      createdAt: new Date('2025-01-29T04:31:59.000Z'),
+      updatedAt: new Date('2025-02-01T04:53:22.048Z'),
+      id: 1,
+      code: 'PRJ456',
+      title: 'Second Project',
+      description: 'Second sample project',
+      isCompleted: false,
+      createdBy: 1,
+    },
+  });
+
+  await prisma.tasks.create({
+    data: {
+      createdAt: new Date('2025-01-29T04:41:05Z'),
+      updatedAt: new Date('2025-01-29T04:41:05Z'),
+      id: 1,
+      title: 'Implement Feature',
+      description: 'Develop a new feature',
+      reporterId: 1,
+      assigneeId: 2,
+      projectId: 1,
+      typeId: 3,
+      priorityId: 2,
     },
   });
 
